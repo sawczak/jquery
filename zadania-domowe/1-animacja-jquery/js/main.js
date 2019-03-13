567342e18b2f2c98fd36bfc743abf3f037099faf
Izabela Sawczak <izabela.ziemba@gmail.com>
2019-03-13 20:13:03 +0100

    zadania 1 - animacja jquery

--- zadania-domowe/1-animacja-jquery/js/main.js
+++ zadania-domowe/1-animacja-jquery/js/main.js
@@ -1,2 +1,23 @@
-$('body').append('<button class="btn">Przycisk</button');
-$('body').after('<div></div>').css({width: '200px', height: '200px', 'background': 'red'});
+$('body').append('<button class="btn">Kliknij guzik</button>', '<div class="box"></div>').css({'position': 'relative', 'font-size': '12px', 'color': 'gold'});
+$('.box').css({'height': '200', 'width': '200', 'background-color': 'red', 'position': 'absolute'});
+
+    $('.btn').click(function() {
+        $('div').animate({
+          left: '100px'
+        }, 1000 );
+
+        $('.box').animate({
+          height: '100px',
+          width: '100px'
+        }, 2000, zmianaKoloru );
+
+        function zmianaKoloru() {
+            console.log('funkcja działa');
+            $('.box').animate({
+            'background-color': 'blue'
+          }, 5000, dodajNapis );
+        };
+
+        function dodajNapis() {
+            $('.box').html('<h2>Animacja zakończona</h2>')};
+    });
